fdb5198c2b33b63fa51a459d3b5213757d90d8fd
DESKTOP-3BE06MR\ryu <rjch221@gmail.com>
2019-01-15 02:37:23 +0900

    IOCP Client 생성
    - 테스툥으로 기본 JCTcpClinet 생성
    - 구현을 헤더파일에 했음

--- IOCP/IOCP/JCTcpServer.h
+++ IOCP/IOCP/JCTcpServer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "IOCP.h"
+#include "JCPacket.h"
 
 class IOCPServer : public IOCP
 {
@@ -10,10 +11,7 @@ public:
 
 	virtual void OnRecvPacket(SocketInfo* socketInfo, char* packet);
 	virtual void OnCloseSocket();
-
 };
-
-
 
 class JCTcpServer
 {
@@ -24,3 +22,50 @@ public:
 	~JCTcpServer();
 };
 
+class IOCPClient : public IOCP
+{
+public:
+	IOCPClient()
+	{
+		Connect("127.0.0.1", 3302);
+	}
+	~IOCPClient()
+	{
+
+	}
+
+	virtual void OnRecvPacket(SocketInfo* socketInfo, char* packet)
+	{
+		fprintf(stdout, "OnRecvPacket in client\n");
+		InputStream istrm(packet, 108);
+		JCPacket in;
+		in.StreamToObject(istrm);
+	}
+	virtual void OnCloseSocket()
+	{
+		fprintf(stdout, "OnCloseSocket in client\n");
+	}
+
+};
+
+class JCTcpClient
+{
+private:
+	IOCPClient proxy;
+public:
+	JCTcpClient() {}
+	~JCTcpClient() {}
+
+	void Send()
+	{
+		JCHeader h(JCHeader::Type::RES, sizeof(JCHeader) + sizeof(JCSysinfo));
+		JCSysinfo s("test_anem", "test_dir");
+		JCPacket p(h, s);
+
+		OutputStream ostrm;
+		p.ObjectToStream(ostrm);
+
+
+		proxy.Send(ostrm.GetBufferPtr());
+	}
+};
